ca55f39acb622c360d5956712db4420060b63050
alim-2023 <alim@crystallitedigital.com>
2023-10-25 22:46:37 +0500

    fixes

--- components/Awards.js
+++ components/Awards.js
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import icon23 from '../public/images/newawards/1.webp'
 import icon24 from '../public/images/newawards/2.webp'
 import icon25 from '../public/images/newawards/3.webp'
-import icon26 from '../public/images/newawards/4.webp'
+import icon26 from '../public/images/newawards/44.png'
 import icon27 from '../public/images/newawards/5.webp'
 
 
@@ -28,28 +28,28 @@ const Awards = () => {
                         <div className={styles.fill}>
                             <div>
                                 <Image src={icon23} className='img-fluid' />
-                                <h3 className='white font20 center mt-4 mb-4 line3'>Best of Consumer <br></br> Electronics Show (CES)</h3>
+                                <h3 className='white font20 center mt-4 mb-3 line3'>Best of Consumer <br></br> Electronics Show (CES)</h3>
                                 <h4 className='white font16  center fw300'>DroneRush, 2023</h4>
                             </div>
                             <div>
                                 <Image src={icon24} className='img-fluid' />
-                                <h3 className='white font20 center mt-4 mb-4 line3'>Best Mobile App <br></br> Developers in LA</h3>
+                                <h3 className='white font20 center mt-4 mb-3 line3'>Best Mobile App <br></br> Developers in LA</h3>
                                 <h4 className='white font16  center fw300'>Expertise, 2022</h4>
                             </div>
                             <div>
                                 <Image src={icon25} className='img-fluid' />
-                                <h3 className='white font20 center mt-4 mb-4 line3'>Top 100 Companies <br></br>
+                                <h3 className='white font20 center mt-4 mb-3 line3'>Top 100 Companies <br></br>
                                     on Clutch</h3>
                                 <h4 className='white font16  center fw300'>Clutch, 2021</h4>
                             </div>
                             <div>
                                 <Image src={icon26} className='img-fluid' />
-                                <h3 className='white font20 center mt-4 mb-4 line3'>Forbes <br></br>Business Award</h3>
-                                <h4 className='white font16  center fw300'>Forbes, 2020</h4>
+                                <h3 className='white font20 center mt-4 mb-3 line3'>Forbes <br></br>Business Award</h3>
+                                <h4 className='white font16  center fw300'>Forbes, 2023</h4>
                             </div>
                             <div>
                                 <Image src={icon27} className='img-fluid' />
-                                <h3 className='white font20 center mt-4 mb-4 line3'>Top Creative & Design <br></br> Agencies in California</h3>
+                                <h3 className='white font20 center mt-4 mb-3 line3'>Top Creative & Design <br></br> Agencies in California</h3>
                                 <h4 className='white font16  center fw300'>Clutch, 2019</h4>
                             </div>
                         </div>
