4380d224f37be7cade4ef5a098e60f618ba4f4e8
cl-farooqaziz <dev.faz@production-dev.com>
2023-11-17 00:35:57 +0500

    reduce unused js and css

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
@@ -9,20 +10,35 @@ const nextConfig = {
         hostname: 'wp23.cryscampus.com',
         port: '',
         pathname: '/**',
-      }
-    ]
-  }
-}
-
-rewrites: async () => [
-  {
-    source: "/public/supersale/index.html",
-    destination: "/pages/api/index.js",
+      },
+    ],
   },
-  {
-    source: "/public/topecommerce/index.html",
-    destination: "/pages/api/ecommerce.js",
-  },
-],
+  rewrites: async () => [
+    {
+      source: "/public/supersale/index.html",
+      destination: "/pages/api/index.js",
+    },
+    {
+      source: "/public/topecommerce/index.html",
+      destination: "/pages/api/ecommerce.js",
+    },
+  ],
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer = [new TerserPlugin()];
 
-module.exports = nextConfig
+      config.optimization.splitChunks = {
+        chunks: 'all',
+        cacheGroups: {
+          styles: {
+            name: 'styles',
+            test: /\.css$/,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      };
+    }
+    return config;
+  },
+};
