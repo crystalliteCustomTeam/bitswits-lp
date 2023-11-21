ccd3765b1b24685991973d1a6f17105708091827
cl-farooqaziz <dev.faz@production-dev.com>
2023-11-21 06:00:23 +0500

    set time out

--- pages/_app.js
+++ pages/_app.js
@@ -27,7 +27,7 @@ export default function App({ Component, pageProps }) {
   // =======================================
   const [imagesLoaded, setImagesLoaded] = useState(false);
   useEffect(() => {
-    const delay = 3000;
+    const delay = 5000;
     const timeoutId = setTimeout(() => {
       setImagesLoaded(true);
     }, delay);
