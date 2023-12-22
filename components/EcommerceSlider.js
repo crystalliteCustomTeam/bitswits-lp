ee5325964302a68833f650b70db9e4c19bcad84d
alim-2023 <alim@crystallitedigital.com>
2023-12-22 22:02:22 +0500

    slider auto play

--- components/EcommerceSlider.js
+++ components/EcommerceSlider.js
@@ -6,8 +6,10 @@ import SwiperCore, { Navigation, Autoplay } from 'swiper/core';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/autoplay';
+import Swiper from 'swiper/bundle'; 
 import React, { useEffect, useRef } from 'react';
 // Import images
+import { register } from "swiper/element/bundle";
 
 
 SwiperCore.use([Navigation, Autoplay]);
@@ -15,7 +17,17 @@ SwiperCore.use([Navigation, Autoplay]);
 const EcommerceSlider = () => {
 
 
+    // const swiperPrevSlide = document.querySelector('.swiper-slide-prev');
+    // const swiperEl = document.querySelector('.swiper-container');
 
+    // const activeSlide = this.slides[this.activeIndex];
+
+    // if (swiperPrevSlide) {
+    //     const prevSibling = swiperPrevSlide;
+    //     prevSibling.classList.add('previous-sibling');
+    // }
+
+    
 
 
     const slides = [
@@ -26,7 +38,6 @@ const EcommerceSlider = () => {
         { img1fold: '../../ecommerce/5.webp' },
         { img1fold: '../../ecommerce/6.webp' },
         { img1fold: '../../ecommerce/7.webp' },
-    
         { img1fold: '../../ecommerce/2.webp' },
         { img1fold: '../../ecommerce/3.webp' },
         { img1fold: '../../ecommerce/4.webp' },
@@ -36,57 +47,103 @@ const EcommerceSlider = () => {
 
     ];
 
- 
 
     const swiperElRef = useRef(null);
-
     useEffect(() => {
-        const mySwiper = new SwiperCore(swiperElRef.current, {
-            slidesPerView: 7,
-            centeredSlides: true,
-            loop: true,
-            autoplay: {
-                delay: 5000,
+        register();
+        const params = {
+            slidesPerView: 9,
+            injectStyles: [
+                `.swiper-pagination-bullet {
+            width: 10px;
+            height: 10px;
+          }
+          .swiper-pagination-bullet-active {
+            background: #00C0E4;
+          }
+        `,
+            ],
+            breakpoints: {
+                300: {
+                    slidesPerView: 1,
+                    spaceBetween: 10,
+                },
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 20,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1340: {
+                    slidesPerView: 3,
+                },
             },
-        });
-        // Remove the 'previous-sibling' class from all elements with the class 'swiper-slide'
-        document.querySelectorAll('.swiper-slide').forEach((element) => {
-            element.classList.remove('previous-sibling');
-        });
-
-        // Add the 'previous-sibling' class to the immediate previous sibling's previous sibling of the active slide
-        const activeSlide = mySwiper.slides[mySwiper.activeIndex];
-        const prevSibling = activeSlide.previousElementSibling;
-
-        if (prevSibling && prevSibling.classList.contains('swiper-slide')) {
-            const prevPrevSibling = prevSibling.previousElementSibling;
-            if (prevPrevSibling && prevPrevSibling.classList.contains('swiper-slide')) {
-                prevPrevSibling.classList.add('previous-sibling');
-            }
-        }
-        mySwiper.on('slideChange', () => {
-            // Remove the 'previous-sibling' class from all elements with the class 'swiper-slide'
-            document.querySelectorAll('.swiper-slide').forEach((element) => {
-                element.classList.remove('previous-sibling');
-            });
-
-            // Add the 'previous-sibling' class to the immediate previous sibling's previous sibling of the active slide
-            const activeSlide = mySwiper.slides[mySwiper.activeIndex];
-            const prevSibling = activeSlide.previousElementSibling;
-
-            if (prevSibling && prevSibling.classList.contains('swiper-slide')) {
-                const prevPrevSibling = prevSibling.previousElementSibling;
-                if (prevPrevSibling && prevPrevSibling.classList.contains('swiper-slide')) {
-                    prevPrevSibling.classList.add('previous-sibling');
-                }
-            }
-        });
-
-        // Clean up when the component is unmounted
-        return () => {
-            mySwiper.destroy();
         };
+      
+
+
+        Object.assign(swiperElRef.current, params);
+        swiperElRef.current.initialize();
+
+
+
+      
+
+
+
     }, []);
+
+
+    // const swiperElRef = useRef(null);
+
+    // useEffect(() => {
+    //     const mySwiper = new SwiperCore(swiperElRef.current, {
+    //         slidesPerView: 7,
+    //         centeredSlides: true,
+    //         loop: true,
+    //         autoplay: {
+    //             delay: 5000,
+    //         },
+    //     });
+    //     // Remove the 'previous-sibling' class from all elements with the class 'swiper-slide'
+    //     document.querySelectorAll('.swiper-slide').forEach((element) => {
+    //         element.classList.remove('previous-sibling');
+    //     });
+
+    //     // Add the 'previous-sibling' class to the immediate previous sibling's previous sibling of the active slide
+    //     const activeSlide = mySwiper.slides[mySwiper.activeIndex];
+    //     const prevSibling = activeSlide.previousElementSibling;
+
+    //     if (prevSibling && prevSibling.classList.contains('swiper-slide')) {
+    //         const prevPrevSibling = prevSibling.previousElementSibling;
+    //         if (prevPrevSibling && prevPrevSibling.classList.contains('swiper-slide')) {
+    //             prevPrevSibling.classList.add('previous-sibling');
+    //         }
+    //     }
+    //     mySwiper.on('slideChange', () => {
+    //         // Remove the 'previous-sibling' class from all elements with the class 'swiper-slide'
+    //         document.querySelectorAll('.swiper-slide').forEach((element) => {
+    //             element.classList.remove('previous-sibling');
+    //         });
+
+    //         // Add the 'previous-sibling' class to the immediate previous sibling's previous sibling of the active slide
+    //         const activeSlide = mySwiper.slides[mySwiper.activeIndex];
+    //         const prevSibling = activeSlide.previousElementSibling;
+
+    //         if (prevSibling && prevSibling.classList.contains('swiper-slide')) {
+    //             const prevPrevSibling = prevSibling.previousElementSibling;
+    //             if (prevPrevSibling && prevPrevSibling.classList.contains('swiper-slide')) {
+    //                 prevPrevSibling.classList.add('previous-sibling');
+    //             }
+    //         }
+    //     });
+
+    //     // Clean up when the component is unmounted
+    //     return () => {
+    //         mySwiper.destroy();
+    //     };
+    // }, []);
 
 
 
@@ -96,9 +153,9 @@ const EcommerceSlider = () => {
         <section className={styles.slider}>
             <Row className='ecommerceslide'>
                 <Col lg={12}>
-                 
 
-                    <div className="swiper-container" ref={swiperElRef}>
+
+                    {/* <div className="swiper-container" ref={swiperElRef}>
                         <div className="swiper-wrapper">
                             {
                                 slides?.map((e, i) => (
@@ -114,7 +171,35 @@ const EcommerceSlider = () => {
                                 ))
                             }
                         </div>
-                    </div>
+                    </div> */}
+
+                    <swiper-container
+                        init={false}
+                        ref={swiperElRef}
+                        centered-slides="false"
+                        autoplay-delay="3000"
+                        loop="true"
+
+
+                    >
+                        {slides &&
+                            slides.map((testimonial, index) => (
+                                <swiper-slide class='newfold' key={index}>
+                                    <div>
+                                        <div className="w-[90%] m-auto py-10">
+                                            <div className="card testimonialscard">
+                                                <img quality={95}
+                                                    src={testimonial.img1fold}
+                                                    alt='bitswits'
+
+                                                />
+
+                                            </div>
+                                        </div>
+                                    </div>
+                                </swiper-slide>
+                            ))}
+                    </swiper-container>
                 </Col>
             </Row>
         </section>
