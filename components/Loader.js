6b160ed9c4a6cd9b99a2f4b3e4da4ae3e8733cfe
cl-farooqaziz <dev.faz@production-dev.com>
2023-12-28 00:03:39 +0500

    loader video added

--- components/Loader.js
+++ components/Loader.js
@@ -6,8 +6,7 @@ const Loader = () => {
     return (
         <>
             <section className={styles.loader}>
-                {/* <video autoPlay loop muted src='https://player.vimeo.com/progressive_redirect/playback/898148403/rendition/1080p/file.mp4?loc=external&log_user=0&signature=659acc51d9cd725d6ccec0e7372a8867e5b92329b955b621995411325692ed43' className='w-50 h-50'></video> */}
-                <p>BitsWits</p>
+                <video autoPlay loop muted src='https://player.vimeo.com/progressive_redirect/playback/898148403/rendition/1080p/file.mp4?loc=external&log_user=0&signature=659acc51d9cd725d6ccec0e7372a8867e5b92329b955b621995411325692ed43' className='w-50 h-50'></video>
             </section>
         </>
     )
