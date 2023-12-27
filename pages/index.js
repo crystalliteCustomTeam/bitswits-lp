d4b9c17712f2cfe3d2a8b5274ae6da5e9929cd41
cl-farooqaziz <dev.faz@production-dev.com>
2023-12-27 23:53:48 +0500

    optimization fixes

--- pages/index.js
+++ pages/index.js
@@ -166,14 +166,25 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Top Mobile App Development Company - BitsWits</title>
-        <meta
-          name="description"
-          content="Apps that simplify your life, one click at a time, BitsWits is your go-to destination for top mobile app developers who can help you bring your ideas to life."
-        />
+      <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Title & Description */}
+        <title>Top Mobile App Development Company - Bitswits.</title>
+        <meta name="description" content="Apps that simplify your life, one click at a time, BitsWits is your go-to destination for top mobile app developers who can help you bring your ideas to life." />
+        {/* OG Tags */}
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Top Mobile App Development Company - Bitswits." />
+        <meta property="og:description" content="Apps that simplify your life, one click at a time, BitsWits is your go-to destination for top mobile app developers who can help you bring your ideas to life." />
+        <meta property="og:url" content="https://bitswits.co/" />
+        <meta property="og:site_name" content="Top Mobile App Development Company - Bitswits." />
+        {/* Canonical */}
+        <link rel="canonical" href="https://bitswits.co/" />
+        {/* Favicon */}
         <link rel="icon" href="images/icons/favicon.png" />
       </Head>
+      
       <NewIndexBanner />
       <section>
         <div className={`${styles.awardsFold}`}>
