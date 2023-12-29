59dd8a6f3a0045b673dcbf4aa32adfde817830e7
alim-2023 <alim@crystallitedigital.com>
2023-12-29 22:22:54 +0500

    newrev.5

--- pages/_app.js
+++ pages/_app.js
@@ -16,10 +16,13 @@ import EcommerceBanner from "@/components/EcommerceBanner";
 import EcommerceFooter from "@/components/EcommerceFooter";
 import Cursor from "@/components/Cursor";
 import Loader from "@/components/Loader";
-
+import tel from "@/public/images/fixed/tel.webp"
+import call from "@/public/images/fixed/call.webp"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function App({ Component, pageProps }) {
-  
+
   const router = useRouter();
   // =======================================
   const [imagesLoaded, setImagesLoaded] = useState(false);
@@ -98,8 +101,69 @@ export default function App({ Component, pageProps }) {
     router.pathname == "/top-ecommerce-app-development-company";
   // =======================================
 
+  const [isHovered, setIsHovered] = useState(false);
+
+  function loc() {
+    setIsHovered((prev) => !prev);
+  }
+ 
+
   return (
     <>
+
+      <div className="newtownfy">
+        <div className="chat">
+          <span className="icon">
+            <Image width="30" height="30" alt="bitswits" className="img-fluid" src={tel} loading="lazy" />
+            <div className="txtBody">
+              <Link href="#">Chat Now</Link>
+            </div>
+          </span>
+        </div>
+        <div className="call">
+          <span className="icon">
+            <Image width="30" height="30" alt="bitswits" className="img-fluid" src={call} loading="lazy" />
+            <div className="txtBody">
+              <Link href="tel:13123795987">1 - 312 379 5987</Link>
+            </div>
+          </span>
+        </div>
+      </div>
+
+
+      <div className={isHovered  ? 'openForm active' : 'openForm'}>
+        <div className="wrapper">
+          <span className="close" onClick={() => loc()}>
+            {isHovered  ?
+
+              'x'
+              :
+              ' Get In Touch'
+            }
+
+          </span>
+          <form id="leadPopupForm" className="popup-form" data-hs-cf-bound="true">
+            <div className="inputGroup">
+              <h3 className="f-20 center fw800">Get A Free Quote Now!</h3>
+            </div>
+            <div className="inputGroup">
+              <input name="name" type="text" placeholder="Full Name*" required="" />
+            </div>
+            <div className="inputGroup">
+              <input name="email" type="email" placeholder="Email Address*" required="" />
+            </div>
+            <div className="inputGroup">
+              <input type="phone" required="" minlength="7" maxlength="15" name="phone" placeholder="Phone No*" onkeypress="return /[0-9]/i.test(event.key)" />
+            </div>
+            <div className="inputGroup">
+              <textarea name="comments" placeholder="Let Us know Time And Date To Call You."></textarea>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+
+
       {newlps ? (
         <Headerlp />
       ) : newhomepage ? (
