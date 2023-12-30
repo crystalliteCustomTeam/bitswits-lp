f24ae16fd4ad3d183ab7722db19bd429c0ab9cdf
cl-farooqaziz <dev.faz@production-dev.com>
2023-12-30 05:52:11 +0500

    G-tags & Conversion-tag added

--- pages/thank-you.js
+++ pages/thank-you.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Script from "next/script";
 import { Container, Row, Col } from "react-bootstrap";
@@ -8,6 +8,16 @@ import styles from "@/styles/Newhomeanner.module.css";
 
 
 export default function ThankYou() {
+
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+  useEffect(() => {
+    const delay = 7000;
+    const timeoutId = setTimeout(() => {
+      setImagesLoaded(true);
+    }, delay);
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
     <>
       <Head>
@@ -20,23 +30,22 @@ export default function ThankYou() {
         <link rel="icon" href="images/icons/favicon.png" />
       </Head>
 
-      {/* <Script>
-        {`gtag('event', 'conversion', {'send_to': 'AW-11114809734/O9snCOO5m5EYEIaT-rMp'});`}
-        </Script> */}
-
-
-      {/* Google tag (gtag.js) */}
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11114809734"></Script>
-      <Script>
-        {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'AW-11114809734')`};
-      </Script>
-      {/* <!-- Event snippet for Website lead conversion page --> */}
-      <Script>
-        {`gtag('event', 'conversion', {'send_to': 'AW-11114809734/O9snCOO5m5EYEIaT-rMp'});`}
-      </Script>
+      {imagesLoaded &&
+        <>
+          {/* Google tag (gtag.js) */}
+          <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=AW-11114809734"></Script>
+          <Script>
+            {`window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-11114809734')`};
+          </Script>
+          {/* <!-- Event snippet for Website lead conversion page --> */}
+          <Script>
+            {`gtag('event', 'conversion', {'send_to': 'AW-11114809734/O9snCOO5m5EYEIaT-rMp'});`}
+          </Script>
+        </>
+      }
 
       <main>
         <section className={styles.thankYou}>
