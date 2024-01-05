da3b152aee0285d5e44f8050092c5ae8bb252558
alim-2023 <alim@crystallitedigital.com>
2024-01-05 18:16:34 +0500

    nrerev.6

--- components/uiuxdesigndevelopment/Smarteruiux.js
+++ components/uiuxdesigndevelopment/Smarteruiux.js
@@ -35,6 +35,7 @@ const Provideservices = (props) => {
     autoplay: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    adaptiveHeight: true,
     responsive: [
       {
         breakpoint: 1200,
